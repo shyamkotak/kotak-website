a196f5bc13d12d54202d684e0792ced362df43f3
shyamkotak <me@shyamkotak.com>
2019-04-14 21:27:43 -0400

    allow icon hover

--- src/home/components/Icon.js
+++ src/home/components/Icon.js
@@ -7,11 +7,9 @@ function Icon(props){
   const styles_icon = {'color':props.color}
 
   return (
-    <a href={props.link} target="_blank" rel="noopener noreferrer">
-      <span className="fa-stack fa-2x">
+    <a className="fa-stack icon" href={props.link} target="_blank" rel="noopener noreferrer">
         <FontAwesomeIcon style={styles_box} className="fa-stack-2x" icon={["far", "circle"]}/>
         <FontAwesomeIcon style={styles_icon} className="fa-stack-1x" icon={[props.iclass, props.icon]}/>
-      </span>
     </a>
   );
 
